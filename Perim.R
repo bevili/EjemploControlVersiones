b2030e8b6df7ce4a41835d0cf4705874597277e4
bevili <bevili@alumni.uv.es>
2026-02-08 11:20:56 +0100

    ajustes previos

---
+++ Perim.R
@@ -0,0 +1,12 @@
+PerimetroCuadrado <- function(L) {
+  return(4*L)
+}
+
+LongitudCircunf <- function(R) {
+  2 * 3.14159 * R
+}
+
+
+PerimetroRectangulo <- function(base, altura) {
+  2 * (base + altura)
+}
